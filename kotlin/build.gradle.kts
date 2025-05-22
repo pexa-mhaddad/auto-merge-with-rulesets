d1ec6501af24c92cf67f91a3c0bf7736454c6b29
pexa-mhaddad <160090720+pexa-mhaddad@users.noreply.github.com>
2025-05-22 15:13:23 +1000

    init

---
+++ kotlin/build.gradle.kts
@@ -0,0 +1,17 @@
+plugins {
+    kotlin("jvm") version "2.1.0"
+    application
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib")
+    implementation("com.squareup.okhttp3:okhttp:4.9.0")
+}
+
+application {
+    mainClass.set("MainKt")
+}
